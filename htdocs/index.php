5e512f314f1898a40ff7376d931ad266623d3a79
Guilherme alves <103002795+Guilhermealves0308@users.noreply.github.com>
2022-09-26 19:24:31 -0300

    Add files via upload

---
+++ htdocs/index.php
@@ -0,0 +1,68 @@
+<?php 
+require($_SERVER['DOCUMENT_ROOT'] . '/header.php');
+?>
+
+                <main>
+
+                    <article>
+                        <section class="flex">
+                            <div>
+                                <div class="card">
+                                    <img src="/img/gta.jpg" alt="Denim Jeans" style="width:100%">
+                                    <h1></h1>
+                                    <p class="price">$19.99</p>
+                                    <p></p>
+                                    <p><button>Add to Cart</button></p>
+                                  </div>
+                            </div>
+                            <div>
+                            <div class="card">
+                                    <img src="/img/gta.jpg" alt="Denim Jeans" style="width:100%">
+                                    <h1></h1>
+                                    <p class="price">$19.99</p>
+                                    <p></p>
+                                    <p><button>Add to Cart</button></p>
+                                  </div>
+                            </div>
+                            <div>
+                            <div class="card">
+                                    <img src="/img/gta.jpg" alt="Denim Jeans" style="width:100%">
+                                    <h1></h1>
+                                    <p class="price">$19.99</p>
+                                    <p></p>
+                                    <p><button>Add to Cart</button></p>
+                                  </div>
+                            </div>
+                            <div>
+                            <div class="card">
+                                    <img src="/img/gta.jpg" alt="Denim Jeans" style="width:100%">
+                                    <h1></h1>
+                                    <p class="price">$19.99</p>
+                                    <p></p>
+                                    <p><button>Add to Cart</button></p>
+                                  </div>
+                            </div>
+                            <div>
+                            <div class="card">
+                                    <img src="/img/gta.jpg" alt="Denim Jeans" style="width:100%">
+                                    <h1></h1>
+                                    <p class="price">$19.99</p>
+                                    <p></p>
+                                    <p><button>Add to Cart</button></p>
+                                  </div>
+                            </div>
+                            <div>
+                            <div class="card">
+                                    <img src="/img/gta.jpg" alt="Denim Jeans" style="width:100%">
+                                    <h1></h1>
+                                    <p class="price">$19.99</p>
+                                    <p></p>
+                                    <p><button>Add to Cart</button></p>
+                                  </div>
+                            </div>
+                        </section>
+                    </article>
+                </main>
+                <?php
+                require($_SERVER['DOCUMENT_ROOT'] . '/footer.php')
+                ?>
